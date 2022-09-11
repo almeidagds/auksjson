bd713c47064ee07e76eafc0d33e8fb66681bec9f
Gabriel de Almeida <almeidag2001@gmail.com>
2022-09-11 15:26:48 -0300

    Adicionando interface 'ILeilaoDao' e renomeando classe 'LeilaoDao'.

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/ILeilaoDao.cs
@@ -0,0 +1,22 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Alura.LeilaoOnline.WebApp.Models;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public interface ILeilaoDao
+    {
+        public IEnumerable<Categoria> BuscarCategorias();
+
+        public IEnumerable<Leilao> BuscarLeiloes();
+
+        public Leilao BuscarPorId(int id);
+
+        public void Incluir(Leilao leilao);
+
+        public void Alterar(Leilao leilao);
+
+        public void Excluir(Leilao leilao);
+    }
+}

96439308df76b150788ce8af55d61628d4b1e7eb
Gabriel de Almeida <almeidag2001@gmail.com>
2022-09-12 21:48:16 -0300

    Desenvolvendo interfaces de serviço.

--- src/Alura.LeilaoOnline.WebApp/Services/IProdutoService.cs
+++ src/Alura.LeilaoOnline.WebApp/Services/IProdutoService.cs
@@ -1,6 +1,14 @@
-﻿namespace Alura.LeilaoOnline.WebApp.Services
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Services
 {
     public interface IProdutoService
     {
+        IEnumerable<Leilao> PesquisaLeiloesEmPregaoPorTermo(string termo);
+
+        IEnumerable<CategoriaComInfoLeilao> ConsultarCategoriasComTotalDeLeiloesEmPrego();
+
+        Categoria ConsultaCategoriaPorIdComLeiloesEmPregao(int id);
     }
 }
